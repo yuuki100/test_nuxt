5ab9bb2d0d0fce378ccdd2d6e3512ffe43ff9be7
yuuki <relaxation11y@gmail.com>
2022-09-21 22:06:44 +0900

    update

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!--<Tutorial/>-->
+  <div class="container">
+    <h1 class="title">
+      Hello World hoge
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'IndexPage'
+}
+</script>
